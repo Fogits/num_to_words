470fecef4a5568d356eba7afe8233d16797c2c5e
imed.chaabouni <imed.chaabouni@fogits.com>
2019-02-20 14:40:42 +0100

    add summary to manifest file

--- num_to_words/__manifest__.py
+++ num_to_words/__manifest__.py
@@ -3,6 +3,8 @@
     'name': 'Convert number to words',
     'version': '12.0',
     'author': "Fogits Solutions",
+    'summary': """
+    This module allows you to convert number to words """,
     'website': "https://www.fogits.com/",
     'description': """
         Convert invoice amount to text 
