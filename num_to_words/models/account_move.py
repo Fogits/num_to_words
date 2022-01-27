1e67fc353e5cce6dff4271e90f43ef990670d27e
Hind BAHLOUL <hind.bahloul@fogits.com>
2022-01-27 15:09:15 +0100

    [update_account_move]

--- num_to_words/models/account_move.py
+++ num_to_words/models/account_move.py
@@ -12,5 +12,13 @@ class AccountMove(models.Model):
     @api.depends('amount_total')
     def amount_to_words(self):
         if self.company_id.text_amount_language_currency:
-            self.text_amount = num2words(self.amount_total, to='currency',
-                                         lang=self.company_id.text_amount_language_currency)
+            amount_i, amount_d = divmod(self.amount_total, 1)
+            amount_d = int(round(amount_d * 1000,2))
+            result1 = num2words(amount_i,lang=self.company_id.text_amount_language_currency)
+            result2=num2words(amount_d,lang=self.company_id.text_amount_language_currency)
+            self.text_amount = '%(result1)s %(currency_unit)s %(result2)s %(currency_subunit)s' % {
+                'result1': result1,
+                'result2': result2,
+                'currency_unit':  self.currency_id.currency_unit_label,
+                'currency_subunit':self.currency_id.currency_subunit_label
+            }
